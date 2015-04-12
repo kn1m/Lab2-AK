82531a9959e5edff2383c2e6ff7a285c88839f53
kn1m <peteknysh@gmail.com>
2015-04-12 13:55:58 +0300

    mb finished, didn't tested

--- core.py
+++ core.py
@@ -2,47 +2,91 @@
 from flask import Flask, jsonify, render_template, request
 app = Flask(__name__,  static_url_path='')
 
+Number = 120
 UsersOnline = 0
-
-
-@app.route('/_add_numbers')
-def add_numbers():
-    """Add two numbers server side, ridiculous but well..."""
-    a = request.args.get('a', 0, type=int)
-    b = request.args.get('b', 0, type=int)
-    return jsonify(result=a + b + 100)
+CurrentUsers = []
+FirstPrime = 0
+BeginCompute = True
+EndOfCurrentPiece = Number + 5
+isFinished = False
+Result = None
 
 
 @app.route('/online')
 def online():
-    # count online users
+    """Returns number of online clients."""
+    global UsersOnline
+    print 'AJAX request to get online user count'
     return jsonify(result=UsersOnline)
 
 
 @app.route('/calculate_current')
 def calculate():
-    # data to worker
-    return jsonify(result=15000000)
+    """Return borders of calculation."""
+    print 'AJAX request to get current data and begin compute'
+    global UsersOnline
+    global BeginCompute
+    global Number
+    if BeginCompute:
+        first = Number
+        second = -1
+        BeginCompute = False
+    if not BeginCompute:
+        first = EndOfCurrentPiece
+        second = EndOfCurrentPiece + 5
+    return jsonify(first_border=first, second_border=second)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    """Renders main page."""
+    global isFinished
+    if isFinished:
+        return 'Computation is finished. Nothing to do.'
     return render_template('index.html')
 
 
 @app.route('/print', methods=['POST'])
 def receive_data():
+    """Return data of computation."""
     received_data = request.json
-    print 'AJAX POST data received from worker'
-    print received_data
+    # worker made his job so decrement online users
+    global UsersOnline
+    UsersOnline -= 1
+    print 'AJAX POST data received from worker: ', received_data
     return jsonify(result=received_data)
 
 
-@app.before_request
-def mark_user():
+@app.route('/watch_worker', methods=['POST'])
+def watch_worker():
+    """Watch current worker state."""
+    received_data = request.json
+    global CurrentData
+    CurrentData = received_data
+    print 'AJAX POST current computation data received from worker: ', received_data
+    return jsonify(result=received_data)
+
+
+@app.route('/mark_online', methods=['POST'])
+def mark_online():
+    """Marks client online."""
+    user_id = request.remote_addr
+    # mark user, before worker started
+    global CurrentUsers
+    CurrentUsers.append(user_id)
     global UsersOnline
     UsersOnline += 1
-    print UsersOnline
+    print 'AJAX POST client registered: ', user_id
+    return jsonify(result=user_id)
+
+
+@app.before_request
+def check_finish():
+    """Checked if finished."""
+    global isFinished
+    global Result
+    if isFinished:
+        print 'Computation is finished. Result: ', Result
 
 if __name__ == "__main__":
     app.run()
--- static/worker.js
+++ static/worker.js
@@ -1,12 +1,14 @@
 self.addEventListener('message', function(e) {
-    var n = e.data;
+    var n = e.data[0];
     search: while (true) {
       n += 1;
-      for (var i = 2; i <= Math.sqrt(n); i += 1)
-        if (n % i == 0)
-         continue search;
-      // found a prime!
-      self.postMessage(n);
-        break;
+        if( n <= e.data[1]) {
+            for (var i = 2; i <= Math.sqrt(n); i += 1)
+                if (n % i == 0)
+                    continue search;
+            self.postMessage(n);
+            break;
+        }
+        self.postMessage(0);
     }
 }, false);
